a1133306dfcd4d81c7c6417517f4855ce678297b
mihyeon.lee <jinsu.na@the-aro.com>
2023-12-21 12:05:18 +0900

    -- 2023.12.21 - 검색시 엔진값 추가 - 구글렌즈 검색시 썸네일 이미지파일로 변환 - 검색 데이터 시각화 통계

---
+++ src/main/java/com/nex/search/entity/UserSearchHistoryDtoInterface.java
@@ -0,0 +1,18 @@
+package com.nex.search.entity;
+
+import java.sql.Timestamp;
+
+public interface UserSearchHistoryDtoInterface {
+    Integer getTsiUno();
+    String getTsiUserFile();    // 대상자
+    String getMonitoringCnt();  // 모니터링 개수
+    String getDeleteReqCnt();   // 삭제요청 개수
+    String getDeleteConfirmCnt();   // 삭제완료 개수
+    String getAllDayMonitoringYn(); // 24시 모니터링 유무
+    String getAllTimeCnt();         // 24시 모니터링 횟수
+    String getResultMonitoringTime(); // 24시 모니터링 시간
+    String getReDsmnCnt();      // 24시 모니터링 재유포 건수
+    String getResultCnt();      // 총 검색 결과 수
+
+
+}
