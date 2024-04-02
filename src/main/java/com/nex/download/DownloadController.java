7fa146b355d1f2abae8a32eeda0afa1026fd8659
eemihyeon <mihyeon.lee@the-aro.com>
2024-04-02 16:11:29 +0900

    1. 이미지 다운로드 구현 중

--- src/main/java/com/nex/download/DownloadController.java
+++ src/main/java/com/nex/download/DownloadController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,25 +20,27 @@ public class DownloadController {
     private final VideoInfoRepository videoInfoRepository;
 
 
-    @GetMapping("/download/zip")
-    public void downloadZip(HttpServletResponse response, Integer tsiUno) {
+    @GetMapping("/download/zip/{tsiUno}")
+    public void downloadZip(HttpServletResponse response,
+                            @PathVariable("tsiUno") Integer tsiUno) {
         DownloadDto downloadDto = new DownloadDto();
         downloadDto.setZipFileName(tsiUno+"_image");
 
         List<VideoInfoEntity> imageList =videoInfoRepository.findAllByTsiUno(tsiUno);
         List<String> downloadFileList = new ArrayList<>();
 
-        for (VideoInfoEntity videoInfoEntity : imageList) {
-            downloadFileList.add(videoInfoEntity.getTviImgRealPath() + videoInfoEntity.getTviImgName());
+        if(!imageList.isEmpty()){
+            for (VideoInfoEntity videoInfoEntity : imageList) {
+                downloadFileList.add(videoInfoEntity.getTviImgRealPath() + videoInfoEntity.getTviImgName());
+            }
+
+            downloadDto.setSourceFiles(downloadFileList);
+
+            try {
+                downloadUtil.downloadZip(downloadDto, response);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
-
-        downloadDto.setSourceFiles(downloadFileList);
-
-        try {
-            downloadUtil.downloadZip(downloadDto, response);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
     }
 }
