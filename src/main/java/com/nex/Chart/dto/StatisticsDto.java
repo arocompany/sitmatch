4e2bd362c84969c6aece2f2d14b6e46a92eb12af
DESKTOP-PBGBB2O\sylee <sangyeol.lee@the-aro.com>
2024-04-25 17:43:56 +0900

    챠트 작업 중

---
+++ src/main/java/com/nex/Chart/dto/StatisticsDto.java
@@ -0,0 +1,15 @@
+package com.nex.Chart.dto;
+
+public interface StatisticsDto {
+    String getTsiType();
+    Integer getCnt();
+    String getUserUno();
+    Integer getCntKeyword();
+    Integer getCntKeywordImg();
+    Integer getCntImg();
+    Integer getCntMov();
+    Integer getCntKeywordMov();
+    String getUserId();
+    String getUserNm();
+
+}
